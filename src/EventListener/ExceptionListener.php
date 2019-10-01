7b8e02ed3873e0eeed899a0c6c8f0bddf8b10de9
Ahmed Bodiwala <admin@multicoin.co>
2019-10-01 16:22:10 +0000

    [Exceptions] Add Global Exception Handler

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getException();
+
+        $response = new JsonResponse([
+            'success' => false,
+            'errors' => [
+                $exception->getStatusCode(),
+            ]
+        ]);
+
+        // HttpExceptionInterface is a special type of exception that
+        // holds status code and header details
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // sends the modified response object to the event
+        $event->setResponse($response);
+    }
+}
